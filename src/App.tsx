8c8643766d7909939b7d6e248a4ac5ea5027b2cf
nguyenquy42 <nguyenquy26012014@gmail.com>
2024-02-20 23:31:52 +0700

    first gsap

--- src/App.tsx
+++ src/App.tsx
@@ -1,37 +1,57 @@
-import reactLogo from "./assets/react.svg";
-import "./App.css";
+import { useRef } from "react";
 import gsap from "gsap";
-import { ScrollTrigger } from "gsap/ScrollTrigger";
-import { useRef, useEffect } from "react";
-gsap.registerPlugin(ScrollTrigger);
+import { useGSAP } from "@gsap/react";
 
-function App() {
-  const imgRef = useRef(null);
-  useEffect(() => {
-    const el = imgRef.current;
-    gsap.fromTo(
-      el,
-      { rotation: 0 },
-      {
-        rotation: 180,
-        duration: 3,
-        scrollTrigger: {
-          trigger: el,
-        },
-      }
-    );
-  }, []);
-  return (
-    <>
-      <div className="helper"></div>
-      <img
-        src={reactLogo}
-        className="logo react"
-        alt="React logo"
-        ref={imgRef}
-      />
-    </>
+const App = () => {
+  const container = useRef(null);
+  useGSAP(
+    () => {
+      const t1 = gsap.timeline();
+      const boxes = gsap.utils.toArray(".title");
+      t1.from("#intro-slider", {
+        xPercent: "-100",
+        duration: 1.3,
+        delay: 0.3,
+      })
+        .from(boxes, {
+          opacity: 0,
+          y: "+=30",
+          stagger: 0.5,
+        })
+        .to(boxes, {
+          opacity: 0,
+          y: "-=30",
+          delay: 0.3,
+          stagger: 0.5,
+        })
+        .to("#intro-slider", {
+          xPercent: "-100",
+          duration: 1.3,
+        })
+        .from("#welcome", {
+          opacity: 0,
+          duration: 0.5,
+        });
+    },
+    { scope: container }
   );
-}
+  return (
+    <div className="relative" ref={container}>
+      <div
+        id="intro-slider"
+        className="absolute top-0 left-0 bg-gray-200 w-full h-screen p-10 flex flex-col gap-4"
+      >
+        <p className="title text-5xl text-gray-900">Fron-End</p>
+        <p className="title text-5xl text-gray-900">Fron-End</p>
+        <p className="title text-5xl text-gray-900">Fron-End</p>
+      </div>
+      <div className="bg-gray-950 h-screen flex flex-col justify-center items-center">
+        <h1 id="welcome" className="text-gray-100 text-5xl">
+          Welcome.
+        </h1>
+      </div>
+    </div>
+  );
+};
 
 export default App;
